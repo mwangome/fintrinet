ecc6cd5e72578e1d7c2d3307c7179437002e6512
Derek Mwachinga <dmwachinga@gmail.com>
2021-05-05 15:34:24 +0300

    under maintenance

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('maintenance');
+    // return view('welcome');
 });
 
 Route::get('/about', function () {
