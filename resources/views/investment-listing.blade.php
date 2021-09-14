f23dcabae947c42f7688273448772f679903acd8
Derek Mwachinga <dmwachinga@gmail.com>
2021-09-14 08:57:47 +0300

    padding table

--- resources/views/investment-listing.blade.php
+++ resources/views/investment-listing.blade.php
@@ -834,15 +834,15 @@
 
             <table>
                 <tr>
-                    <th class="twenty">Entity Code</th>
+                    <th class="twenty" style="padding-left: 10px">Entity Code</th>
                     <th class="eigthy">AGRO (KES153)</th>
                 </tr>
                 <tr>
-                    <td class="twenty">Business Overview</td>
+                    <td class="twenty" style="padding-left: 10px">Business Overview</td>
                     <td>Based in Muranga County, this entity is a leading animal feed manufacturing company with a core focus of improving agricultural production in Kenya. It provides a wide range of feeds and licks specially formulated to boost growth and improve production for farm animals. Amongst its many accolades, the company was recognised as one of the Top100 SME’s in Kenya in 2019.</td>
                 </tr>
                 <tr>
-                    <td class="twenty">Investment Opportunity: </td>
+                    <td class="twenty" style="padding-left: 10px">Investment Opportunity: </td>
                     <td><strong>Business Stage:</strong> <input type="checkbox" id="early-stage" name="early_stage" value="Early Stage"><label for="early-stage"> Early Stage</label>&nbsp;&nbsp;&nbsp;<input type="checkbox" id="sme-stage" name="sme_stage" value="SME" checked><label for="sme-stage"> SME</label>&nbsp;&nbsp;&nbsp;<input type="checkbox" id="large-stage" name="large_stage" value="Large">&nbsp;&nbsp;&nbsp;<label for="large-stage">Large</label><br>
                         <strong>Preferred Instrument:</strong> <input type="checkbox" id="grant" name="grant" value="Grant"><label for="grant"> Grant</label>&nbsp;&nbsp;&nbsp;<input type="checkbox" id="debt" name="debt" value="Debt" checked><label for="debt"> Debt</label>&nbsp;&nbsp;&nbsp;<input type="checkbox" id="quasi-equity" name="quasi_equity" value="Quasi Equity">&nbsp;&nbsp;&nbsp;<label for="quasi-equity">Quasi Equity</label>&nbsp;&nbsp;&nbsp;<input type="checkbox" id="equity" name="equity" value="Equity">&nbsp;&nbsp;&nbsp;<label for="equity">Equity</label><br>
                         <strong>Amount sought (USD):</strong>2,000,000<br>
@@ -850,15 +850,15 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="twenty">Impact: </td>
-                    <td><div class="row"><div class="col-sm-3" style="text-align: center;"><img src="images/hunger.png" alt="" width="100px;" height="100px;"></div><div class="col-sm-9">By providing animal feeds, this entity contributes to improving the productivity and nutrients of livestock production, which in turn enhances the country’s food and nutritional situation. </div></div></td>
+                    <td class="twenty" style="padding-left: 10px">Impact: </td>
+                    <td style="padding-left: 10px"><div class="row"><div class="col-sm-3" style="text-align: center;"><img src="images/hunger.png" alt="" width="100px;" height="100px;"></div><div class="col-sm-9">By providing animal feeds, this entity contributes to improving the productivity and nutrients of livestock production, which in turn enhances the country’s food and nutritional situation. </div></div></td>
                 </tr>
                 <tr>
-                    <td class="twenty">Team: </td>
+                    <td class="twenty" style="padding-left: 10px">Team: </td>
                     <td>The company is governed by a Board of directors and run by a CEO holding a BSc and Msc in Animal Nutrition. </td>
                 </tr>
                 <tr>
-                    <td class="twenty">Financial Snapshot: </td>
+                    <td class="twenty" style="padding-left: 10px">Financial Snapshot: </td>
                     <td><strong>Revenues (USD):</strong> 2,800,000 (June 2021)<br>
                         <strong>Balance sheet size (USD):</strong> 2,740,000<br>
                         <strong>Profitable:</strong> Yes<br>
